f7ca76315022607d95a845f7584134d7d6d6d62b
Cris-Mur <yomy98@gmail.com>
2023-08-18 17:51:47 -0500

    [update][improve locals handling]

--- src/application/config/_locals/index.js
+++ src/application/config/_locals/index.js
@@ -1,32 +1,32 @@
+/**
+ * Module for handling local variables and environment settings.
+ * @module LocalVariables
+ */
+
+const { process_params } = require("express/lib/router");
+
+/**
+ * Represents the local variables object.
+ * @typedef {Object} LocalsObject
+ * @property {Object} locals - A collection of local variables.
+ */
+
 console.log('[Loading locals]');
 
+/**
+ * The object that stores local variables.
+ * @type {LocalsObject}
+ */
 let locals_ojb = {};
 
-Object.keys(process.env).forEach(
-    env_var => {
-        const rex = /locals/i
-        if (rex.test(env_var)) {
-            console.log(env_var);
-            if (env_var === 'LOCALS' && process.env[env_var].length > 1) {
-                let locals_arr = process.env[env_var].split(';');
-                locals_arr.forEach(
-                    locals_var => {
-                        let [key, value] = locals_var.split(':');
-                        if (key.length > 1 && value !== undefined)
-                            locals_ojb[key] = value;
-                    }
-                )
-
-            } else {
-                handleLocalVar(env_var);
-            }
-        }
-    }
-)
-
+/**
+ * Handles a local variable and extracts its value from the environment settings.
+ * @param {string} key - The key of the environment variable.
+ * @returns {Object} An object containing the extracted local variable value.
+ */
 function handleLocalVar(key) {
     let result = {};
-    const rex = /locals/i
+    const rex = /locals/i;
     let split_name = key.split('LOCALS_')[1];
     let raw_value = process.env[key];
     if (rex.test(raw_value)) {
@@ -36,7 +36,15 @@ function handleLocalVar(key) {
     return result;
 }
 
-function splitLocals (locals) {
+/**
+ * Splits a semicolon-separated list of local variable assignments into an object.
+ * @param {string} locals - A string containing semicolon-separated local variable assignments.
+ * @returns {Object} An object with parsed local variables.
+ */
+function splitLocals(locals) {
+    if (!locals) {
+        return {};
+    }
     let local_ = {};
     let splited_locals = locals.split(';');
     for (let local of splited_locals) {
@@ -48,8 +56,23 @@ function splitLocals (locals) {
     return local_;
 }
 
+// Populate locals object with environment variables
+Object.keys(process.env).forEach((env_var) => {
+    locals_ojb = { ...handleLocalVar(env_var) };
+});
+
+// Populate locals object with additional parsed local variables
+locals_ojb = {
+    ...locals_ojb,
+    ...splitLocals(process.env['LOCALS'])
+};
+
 console.log('[obj]', locals_ojb);
 
+/**
+ * Exports the locals object containing all local variables.
+ * @type {LocalsObject}
+ */
 module.exports = {
-    locals : locals_ojb
-}
+    locals: locals_ojb
+};
