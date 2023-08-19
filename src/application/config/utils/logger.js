eb4d9c6f9792033cb7011efb53c6ac5da159a45e
Cris-Mur <yomy98@gmail.com>
2023-08-19 06:35:55 -0500

    [update][add prefix initial logic]

--- src/application/config/utils/logger.js
+++ src/application/config/utils/logger.js
@@ -1,33 +1,56 @@
+/**
+ * Module for customizing console logging and error handling based on the environment.
+ * @module CustomConsole
+ */
+
+/**
+ * Overrides the default `console.log` behavior based on the environment.
+ * @param {...any} args - Arguments to be logged.
+ */
 const raw_log = console.log;
 
 console.log = function (...args) {
     let env = process.env.NODE_ENV;
+    let prefix = '';
     switch (env) {
         case 'production':
-            raw_log(`[${new Date().toISOString()}]`, ...args);
+            prefix += `[${new Date().toISOString()}]`;
+            raw_log(prefix, ...args);
             return;
         case 'development':
-            raw_log(`[development][${new Date().toISOString()}]`, ...args);
+            prefix += `[dev][${new Date().toISOString()}]`;
+            raw_log(prefix, ...args);
             return;
         default:
             raw_log(...args);
     }
 };
 
+/**
+ * Overrides the default `console.error` behavior based on the environment.
+ * @param {...any} args - Arguments to be logged as errors.
+ */
 const raw_error = console.error;
 
 console.error = function (...args) {
     let env = process.env.NODE_ENV;
+    let prefix = '';
     switch (env) {
         case 'production':
-            raw_error(`[${new Date().toISOString()}][ERROR]`, ...args);
+            prefix += `[${new Date().toISOString()}][ERROR]\n`;
+            raw_error(prefix, ...args);
             return;
         case 'development':
-            raw_error(`[development][${new Date().toISOString()}][ERROR]`, ...args);
+            prefix += `[development][${new Date().toISOString()}][ERROR]\n`;
+            raw_error(prefix, ...args);
             return;
         default:
             raw_error(...args);
     }
 };
 
+/**
+ * Exports the customized console with environment-aware logging and error handling.
+ * @type {Object}
+ */
 module.exports = console;
