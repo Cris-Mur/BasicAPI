59d0e0473e7211b7757127644b36e549a5bd8fc3
Cris-Mur <yomy98@gmail.com>
2023-09-30 20:58:56 -0500

    [update][working on logger file feature. this commit saves this code in history but this will remove]

--- src/application/utils/logger/index.js
+++ src/application/utils/logger/index.js
@@ -2,107 +2,46 @@
  * Module for customizing console logging and error handling based on the environment.
  * @module CustomConsole
  */
-
-/**
- * Overrides the default `console.log` behavior based on the environment.
- * @param {...any} args - Arguments to be logged.
- */
-const raw_log = console.log;
-console.log = function logger(...args) {
-    let env = process.env.NODE_ENV;
-    let level=""
-    let prefix = `[${new Date().toISOString()}][${level}]`;
-    switch (env) {
-        case 'production':
-            prefix += ``;
-            raw_log(prefix, ...args);
-            return;
-        case 'development':
-            prefix += `[${env}]`;
-            raw_log(prefix, ...args);
-            return;
-        case 'debug':
-            prefix += `[${env}]`;
-            raw_log(prefix, ...args);
-            return;
-        default:
-            raw_log(...args);
-    }
-};
-/**
- * Overrides the default `console.error` behavior based on the environment.
- * @param {...any} args - Arguments to be logged as errors.
- */
-const raw_error = console.error;
-console.error = function logger_error(...args) {
-    let env = process.env.NODE_ENV;
-    let level="error"
-    let prefix = `[${new Date().toISOString()}][${level}]`;
-    switch (env) {
-        case 'production':
-            prefix += ``;
-            raw_error(prefix, ...args);
-            break;
-        case 'development':
-            prefix += `[${env}]`;
-            raw_error(prefix, ...args);
-            break;
-        case 'debug':
-            prefix += `[${env}]`;
-            raw_error(prefix, ...args);
-            break;
-        default:
-            raw_error(prefix, ...args);
-    }
-    if (true) {
-        error_file = process.env.ERROR_FOLDER + `${"error_nodeapp.log"}`;
-        strToFile(prefix + args.toString() + '\n', debug_file);
-    }
- 
-    if (true) {
-        debug_file = process.env.DEBUG_FOLDER + `${"nodeapp.log"}`;
-        strToFile(prefix + args.toString() + '\n', debug_file);
-    }
- 
-};
-/**
- * Overrides the default `console.error` behavior based on the environment.
- * @param {...any} args - Arguments to be logged as errors.
- */
-const raw_debug = console.debug;
-
-console.debug = async function logger_debug(...args) {
-    let env = process.env.NODE_ENV;
-    let level="debug"
-    let prefix = `[${new Date().toISOString()}][${level}]`;
-    switch (env) {
-        case 'production':
-            prefix += ``;
-            raw_debug(prefix, ...args);
-            break;
-        case 'development':
-            prefix += `[${env}]`;
-            raw_debug(prefix, ...args);
-            break;
-        case 'debug':
-            prefix += `[${env}]`;
-            raw_debug(prefix, ...args);
-            break;
-        default:
-            raw_debug(...args);
-    }
-    if (true) {
-        debug_file = process.env.DEBUG_FOLDER + `${"nodeapp.log"}`;
-        strToFile(prefix + args.toString() + '\n', debug_file);
-    }
-    return;
-};
-
+const path = require('node:path');
 const fs = require('node:fs/promises');
+const boolean = require('../parsers/boolean');
+
+global.logger = boolean.parse(process.env.LOGGER);
+
+const init = require('./init');
+const tag = `[logger]`;
+
+function env() {
+    let env = process.env.NODE_ENV;
+    return `[${env}]`;
+}
+let auxLoggerPath = "";
+(async () => {
+    auxLoggerPath = await init.initLogger();
+})();
+const loggerPath = auxLoggerPath.slice();
+
+async function fileStrategy(level, message) {
+    if (!logger) {
+        return true;
+    }
+    message = message.slice().concat('\n');
+    const prefixLevel = {
+        "log": ".log",
+        "error": ".log.error",
+        "debug": ".log"
+    }
+    let file = await loggerPath + prefixLevel[level];
+    strToFile(message, file);
+}
+
 async function strToFile(string, file) {
     try {
-        console.log('inserting entry to debu file: ', file);
+        console.log('append log to file:', file);
+        console.log('append log:', string);
         await fs.access(file, fs.constants.R_OK | fs.constants.W_OK);
+        await fs.appendFile(file, string);
+        return true;
     } catch (error_) {
         if (error_.code == 'ENOENT') {
             await fs.writeFile(file, string);
@@ -112,9 +51,55 @@ async function strToFile(string, file) {
         console.error('cannot insert entry to debug file.');
         return false;
     }
-    await fs.appendFile(file, string);
-    return true;
 }
+
+
+/**
+ * Overrides the default `console.log` behavior based on the environment.
+ * @param {...any} args - Arguments to be logged.
+ */
+const raw_log = console.log;
+console.log = function logger(...args) {
+    let level="log";
+    let prefix = tag.slice();
+    prefix = prefix.concat(`[${level}]`);
+    prefix = prefix.concat(env());
+    let result = raw_log(prefix, ...args);
+    let message = prefix.slice().concat(args.toString());
+    fileStrategy(level, message)
+}
+/**
+ * Overrides the default `console.error` behavior based on the environment.
+ * @param {...any} args - Arguments to be logged as errors.
+ */
+const raw_error = console.error;
+console.error = function logger_error(...args) {
+    let level="error"
+    /**
+     * {String} - prefix
+     */
+    let prefix = tag.slice();
+    prefix = prefix.concat(`[${level}]`);
+    prefix = prefix.concat(env());
+    let result = raw_error(prefix, ...args);
+    let message = prefix.concat(" ", args.toString());
+    fileStrategy(level, message);
+};
+/**
+ * Overrides the default `console.error` behavior based on the environment.
+ * @param {...any} args - Arguments to be logged as errors.
+ */
+const raw_debug = console.debug;
+
+console.debug = async function logger_debug(...args) {
+    let level="debug"
+    let prefix = tag.slice();
+    prefix = prefix.concat(`[${level}]`);
+    prefix = prefix.concat(env());
+    let result = raw_debug(prefix, ...args);
+    let message = prefix.concat(" ", args.toString());
+    fileStrategy(level, message);
+};
 
 /**
  * Exports the customized console with environment-aware logging and error handling.
