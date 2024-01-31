7ee119d450319786e8f08849da17c14c85716ef6
Cris-Mur <yomy98@gmail.com>
2024-01-31 00:13:15 -0500

    [Add][Add test folder for unit tests]

---
+++ src/application/tests/application.test.js
@@ -0,0 +1,20 @@
+const test = require('node:test');
+const assert = require('node:assert');
+
+const application = require("../app");
+
+test("Class test", (inner_test) => {
+    let test_application = new application();
+    assert.strictEqual(true, test_application instanceof application);
+});
+
+test("Instance test", (inner_test) => {
+    let test_application = new application();
+    assert.notStrictEqual(undefined, test_application.express);
+});
+
+test("Application setup test", (inner_test) => {
+    let test_application = new application();
+    let application_setup_test = application.setup(test_application.express);
+    assert.deepEqual(test_application.express, application_setup_test)
+});
