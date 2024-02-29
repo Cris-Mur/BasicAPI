d2c04769f29a0b899c2a98aa428aa69c96d6d655
Cris-Mur <yomy98@gmail.com>
2024-02-29 07:28:47 -0500

    [update][add some swagger docs for example]

--- src/services/API/home.js
+++ src/services/API/home.js
@@ -6,6 +6,12 @@ router.use('/errors', errors);
 
 /**
  * @swagger
+ * /example:
+ *   get:
+ *     description: Welcome to swagger-jsdoc!
+ *     responses:
+ *       200:
+ *         description: Returns a mysterious string.
  */
 // router.get("/error/test", );
 
--- src/services/router.js
+++ src/services/router.js
@@ -4,7 +4,19 @@ const router = require('express').Router();
  */
 const home = require('./API/home');
 
-
 router.use("/", home);
+
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     description: redirect root endpoint to documentation api
+ *     responses:
+ *       200:
+ *         description: html of swagger.
+ */
+router.get('/', (req, res) => {
+    res.redirect('/api');
+})
 
 module.exports = router;
