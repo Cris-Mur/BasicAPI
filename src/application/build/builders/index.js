079601b18e118e4ff774002e56c9b45cfc84b8f8
Cris-Mur <yomy98@gmail.com>
2024-07-23 17:38:04 -0500

    [Documentation]

--- src/application/build/builders/index.js
+++ src/application/build/builders/index.js
@@ -1,4 +1,22 @@
+/**
+ * @module Index
+ * @description 
+ * This module serves as the entry point for importing and exporting the main 
+ * builders used within the application. It includes the ExpressBuilder 
+ * for creating Express applications and the Builder for other types of builds.
+ * @license MIT
+ * @author @Cris-Mur
+ */
+
+const Builder = require('./builder');
 const ExpressBuilder = require('./Express');
+
+/**
+ * @type {Object}
+ * @property {Builder} Builder - The generic builder module.
+ * @property {ExpressBuilder} ExpressBuilder - The Express application builder module.
+ */
 module.exports = {
+    Builder,
     ExpressBuilder
 };
